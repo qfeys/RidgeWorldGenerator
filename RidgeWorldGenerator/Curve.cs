e03e200cfd01f663285ce88fc5d8dc6a2bbeba1e
qfeys <qfeys@skynet.be>
2019-08-29 21:58:37 +0200

    Moving and scaling

--- RidgeWorldGenerator/Curve.cs
+++ RidgeWorldGenerator/Curve.cs
@@ -61,6 +61,7 @@ namespace RidgeWorldGenerator
         }
 
         public static Vector2 operator +(Vector2 v1, Vector2 v2) => new Vector2(v1.x + v2.x, v1.y + v2.y);
+        public static Vector2 operator -(Vector2 v1, Vector2 v2) => new Vector2(v1.x - v2.x, v1.y - v2.y);
 
         public static Vector2 ZERO => new Vector2(0, 0);
 
--- RidgeWorldGenerator/Form1.cs
+++ RidgeWorldGenerator/Form1.cs
@@ -34,6 +34,36 @@ namespace RidgeWorldGenerator
             Pen pen = new Pen(Color.FromArgb(255, 0, 0, 255));
             curves.ForEach(c=>c.Paint(drawParams, pen, e));
         }
+
+        bool mouseIsDown = false;
+        Point lastMousePos;
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseIsDown = true;
+            lastMousePos = e.Location;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseIsDown == false) return;
+            Point currentMousePos = e.Location;
+            Vector2 delta = new Vector2((currentMousePos.X-lastMousePos.X)/drawParams.scale, (currentMousePos.Y - lastMousePos.Y) / drawParams.scale);
+            drawParams.offset -= delta;
+            lastMousePos = currentMousePos;
+            this.Refresh();
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseIsDown = false;
+        }
+
+        private void Form1_MouseWheel(object sender, MouseEventArgs e)
+        {
+            drawParams.scale *= (float)Math.Pow(1.1, e.Delta / 120);
+            this.Invalidate();
+        }
     }
 
     struct DrawParams
